fa2db4ca3e5a4565e77025dc1aa40ce8e22eef27
ceelosh <marcelo.mdl.dev@gmail.com>
2022-12-29 17:33:17 -0300

    docker :whale:

--- config/config.exs
+++ config/config.exs
@@ -54,6 +54,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :otakuhabu, Otakuhabu.Repo, migration_primary_key: [name: :uuid, type: :binary_id]
+
+config :otakuhabu, Otakuhabu.Repo, migration_primary_key: false
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
